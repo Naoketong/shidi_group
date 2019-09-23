e8c6497711157ef51f6a7904bb1e9a403de1a6fc
xytdz20130731 <103790874@qq.com>
2019-09-23 11:46:27 +0800

    foot

--- src/components/BasicLayout.vue
+++ src/components/BasicLayout.vue
@@ -7,6 +7,77 @@
 			<Brief />
 			<Houses />
 			<Simple />
+
+			<footer class="foot-section">
+				<a href="javascript:;">
+					<img class="footer-option" src="@/img/hamburger.png">
+				</a>
+				<div class="foot-content">
+					<div class="foot-content-top">
+						<div class="foot-top-left">
+							<div class="foot-select">
+								<select class="foot-select-item" name="" id="">
+									<option class="foot-option">实地地产 （总部）</option>
+									<option class="foot-option">实地地产 （总部）</option>
+									<option class="foot-option">实地地产 （总部）</option>
+									<option class="foot-option">实地地产 （总部）</option>
+								</select>
+							</div>
+							<div class="foot-site">
+								<p class="site-hq">
+									广州市天河区华夏路16号富力盈凯广场21楼
+								</p>
+								<p class="site-hq-sub">
+									21st Floor, R&F Yingkai Plaza, 16 Huaxia Road, Tianhe District, Guangzhou
+								</p>
+								<p class="site-hq-phone">020-89192000</p>
+							</div>
+						</div>
+						<div class="foot-top-right">
+							<ul class="foot-blogroll-list">
+								<li class="foot-blogroll-item">
+									<a href="javascript:;">关于实地 ABOUT</a>
+								</li>
+								<li class="foot-blogroll-item">
+									<a href="javascript:;">地产开发 DEVELOPMENT</a>
+								</li>
+								<li class="foot-blogroll-item">
+									<a href="javascript:;">最新消息 LATEST NEWS</a>
+								</li>
+								<li class="foot-blogroll-item">
+									<a href="javascript:;">加入实地 JOIN US</a>
+								</li>
+								<li class="foot-blogroll-item">
+									<a href="javascript:;">廉洁举报 WHISTLEBLOWER</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="foot-content-bottom">
+						<ul class="foot-bottom-list">
+							<li class="foot-bottom-item">
+								<img class="foot-logo" src="@/img/logo-white.png">
+							</li>
+							<li class="foot-bottom-item step2">
+								<span>
+									©2018 版权所有：广州实地房地产开发有限公司.
+									<br>
+									ALL RIGHTS RESERVED: GUANGZHOU SEEDLAND REAL ESTATE DEVELOPMENT CO., LTD.
+									<br>
+									粤ICP备13058296号
+								</span>
+							</li>
+							<li class="foot-bottom-item">
+								<div class="foot-touch">
+									联系 CONTACT
+									<i class="foot-down"></i>
+								</div>
+							</li>
+						</ul>
+					</div>
+					
+				</div>
+			</footer>
 		</div>
 	</div>  
 </template>
